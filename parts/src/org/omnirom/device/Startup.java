e6e9af176adc9e2a6f8cab760fa3611b56c56175
ShevT <evgeny.shishkov@gmail.com>
2018-12-25 20:15:00 +0300

    msm8998-common: Parts: Cleanup
    Change-Id: Ia401c6fa28bb8123c0aa79903826a3fcb4f4092d

--- parts/src/org/omnirom/device/Startup.java
+++ parts/src/org/omnirom/device/Startup.java
@@ -15,33 +15,18 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
+
 package org.omnirom.device;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.v7.preference.PreferenceManager;
 import android.text.TextUtils;
 
 public class Startup extends BroadcastReceiver {
-
-    private void restore(String file, boolean enabled) {
-        if (file == null) {
-            return;
-        }
-        if (enabled) {
-            Utils.writeValue(file, "1");
-        }
-    }
-
-    private void restore(String file, String value) {
-        if (file == null) {
-            return;
-        }
-        Utils.writeValue(file, value);
-    }
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
